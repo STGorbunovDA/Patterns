fb9e3de5251d8cbfb423418ef23d04f40c5b7aaf
Дмитрий Горбунов <bous07@mail.ru>
2023-11-25 20:02:03 +0800

    Add Memento

---
+++ Memento/Memento/GameHistory.cs
@@ -0,0 +1,11 @@
+﻿namespace Memento;
+
+// Caretaker
+class GameHistory
+{
+    public Stack<HeroMemento> History { get; private set; }
+    public GameHistory()
+    {
+        History = new Stack<HeroMemento>();
+    }
+}
